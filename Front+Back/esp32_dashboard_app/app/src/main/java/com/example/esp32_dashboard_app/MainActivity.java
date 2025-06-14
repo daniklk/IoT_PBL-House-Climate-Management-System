b8880e51953b8542266d366161149ac6a1663a42
Greshnick <grisha-kushnr@mail.ru>
2025-06-15 00:24:28 +0300

    added app builder

---
+++ Front+Back/esp32_dashboard_app/app/src/main/java/com/example/esp32_dashboard_app/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.esp32_dashboard_app;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("http://192.168.1.5:5000");
+    }
+}
